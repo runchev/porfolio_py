0cd19178c5fc666218882c1bb576d851d24220b9
runcev <runcev@gmail.com>
2024-02-01 20:35:31 +0100

    Render titles, using pandas

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import streamlit as st
-st.set_page_config(layout="wide")
+import functions
 
-col1, col2 = st.columns(2)
-
+title_row = functions.get_row_from_csv(rowname="title", separator=";")
 info = """
-Hi i am Boro Runchev, designer with 10 years experience.
+Hi i am Boro, designer with 10 years experience.
 
 Excellent knowledge in Adobe Creative Suit application
 InDesign
@@ -24,7 +23,21 @@ Check my portfolio with some of project that i've created.
 Feel free to contact me in any time, about any question that you have.
 """
 
+# st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.png")
 with col2:
+    st.title("Boro Runchev")
     st.info(info)
+
+st.write("<b>Below you can find some of my projects, that i've build in, in my career.</b>", unsafe_allow_html=True)
+
+col3, col4 = st.columns(2)
+with col3:
+    for item in title_row[:10]:
+        st.write("<h3>" + item + "</h3>", unsafe_allow_html=True)
+with col4:
+    for item in title_row[10:]:
+        st.write("<h3>" + item + "</h3>", unsafe_allow_html=True)
