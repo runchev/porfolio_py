031f65212b8a76ce37406a78bbc69ec950f96931
runcev <runcev@gmail.com>
2024-02-02 19:37:51 +0100

    Sending mail using form

--- pages/Contcat_Us.py
+++ pages/Contcat_Us.py
@@ -1,11 +1,12 @@
 import streamlit as st
-
+import send_email as mail_sender
 st.header("Contact me")
 
 with st.form("email_form"):
     st.text_input("Your email", key="email", placeholder="Enter your email here...")
     st.text_area("Your message", key="message", placeholder="Enter your message here")
     submit = st.form_submit_button("Send")
-
     if submit:
-        print("ok")
+        session = st.session_state
+        mail_sender.send_email(session['email'], session['message'])
+        st.info("Your mail was sent successfully")
---
+++ send_email.py
@@ -0,0 +1,22 @@
+import smtplib
+import ssl
+
+
+def send_email(user_mail, message):
+    host = "smtp.gmail.com"
+    port=465
+    username = "runcev@gmail.com"
+    password = "ryei xwgt evxx pesl"
+    context = ssl.create_default_context()
+
+    message_body = f"""\
+Subject: Portfolio mail from {user_mail}
+
+From: {user_mail}
+{message}
+"""
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username,password)
+        server.sendmail(username, user_mail, message_body)
+
